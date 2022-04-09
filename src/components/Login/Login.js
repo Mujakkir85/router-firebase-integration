c95ab3225f38dfa279cd53166213c8eb62a987ae
Mujakkir85 <mujakkirlucse@gmail.com>
2022-04-09 13:10:50 +0600

    added signout use handlesigout and useeffect to know auth state change, in useFirebase page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Login = () => {
+
+    const { singInWithGoogle } = useFirebase()
+
     return (
         <div>
             <h3>Please Login</h3>
+
+            <div style={{ margin: '20px' }}>
+                <button onClick={singInWithGoogle}>Google Sign In</button>
+            </div>
+
+            <form>
+                <input type="email" placeholder='Your Email' /> <br />
+                <input type="password" name="" id="" placeholder='Your Password' /> <br />
+                <input type="submit" value="Login" />
+            </form>
         </div>
     );
 };
---
+++ src/components/Register/Register.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            <h3>Please Register Now</h3>
+
+            <form>
+                <input type="text" placeholder='Your name' /> <br />
+                <input type="email" placeholder='Your Email' /> <br />
+                <input type="password" name="" id="" placeholder='Your Password' /> <br />
+                <input type="submit" value="Register" />
+            </form>
+        </div>
+    );
+};
+
+export default Register;
