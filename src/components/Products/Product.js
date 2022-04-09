8c22b2ee2e908ae74d01057860acae505895cfbb
Mujakkir85 <mujakkirlucse@gmail.com>
2022-04-09 16:09:45 +0600

    sign up by ract firebase hooks and added private route

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -4,14 +4,14 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import app from '../../firebase.init';
 
 const auth = getAuth(app)
-const Home = () => {
-    const [user] = useAuthState(auth);
+
+const Product = () => {
+    const [user] = useAuthState(auth)
     return (
         <div>
-            <h1>This is from home page</h1>
-            <p>Current user is : {user ? user.displayName : 'No Body'}</p>
+            <h2>{user ? user.displayName : 'No One'}</h2>
         </div>
     );
 };
 
-export default Home;
+export default Product;
